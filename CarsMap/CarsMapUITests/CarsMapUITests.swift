8ee46a8e1073c80559472bd9269149993ef4aa76
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-14 13:57:45 +0200

    added a UITest only to have an example

--- CarsMap/CarsMapUITests/CarsMapUITests.swift
+++ CarsMap/CarsMapUITests/CarsMapUITests.swift
@@ -8,27 +8,24 @@
 
 import XCTest
 
-class CarsMapUITests: XCTestCase {
+class CarsMapUITests: XCTestCase
+{
+  var app: XCUIApplication!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUp()
+  {
+    continueAfterFailure = false
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+    app = XCUIApplication()
+    app.launch()
+  }
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+  // Test added only for example
+  func testExample()
+  {
+    app.buttons["CARS LIST"].tap()
+    app.navigationBars["CarsMap.CarsListView"].buttons["Back"].tap()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    XCTAssert(app.buttons["CARS LIST"].exists)
+  }
 }
